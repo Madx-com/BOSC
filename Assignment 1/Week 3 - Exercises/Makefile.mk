595320e4167573291cb44855256bd140f76eff40
Madx_com <Madx.com@gmail.com>
2015-09-18 10:40:57 +0200

    Editted Makefile to work now.

--- Assignment 1/Week 3 - Exercises/Makefile.mk
+++
@@ -1,6 +0,0 @@
-testFB: forback.o testForAndBackground.o
-	gcc -o testFB forback.o testForAndBackground.o
-forback.o: forback.c forback.h
-	gcc -c forback.c
-testForAndBackground.o: testForAndBackground.c
-	gcc -c testForAndBack.c
