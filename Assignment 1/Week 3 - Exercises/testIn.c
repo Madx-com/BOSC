e19187cad6ee3e04e048e41e7122a1a742b15b1b
Madx_com <Madx.com@gmail.com>
2015-09-18 12:47:54 +0200

    Test to redirect

---
+++ Assignment 1/Week 3 - Exercises/testIn.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "redirect.h"
+
+int main(int argc, char *argv[])
+{
+  bool b = false;
+  while(!b)
+  {
+
+    char *p;
+
+    scanf( %c , &p);
+    printf("%s",&p);
+    if('e' == *p)
+    {
+      b = true;
+    }
+  }
+  return 0;
+}
