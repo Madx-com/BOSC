0bde03281a07008b6fc3867cab2d700162f46709
Omar Khan <ok2610@gmail.com>
2015-11-13 10:13:53 +0100

    Initial Assignment 3 Added

---
+++ Assignment 3/Report/Sourcecode.md
@@ -0,0 +1,4 @@
+# Appendix A - Sourcecode
+
+\lstinputlisting{../Sourcecode/main.c}
+
---
+++ Assignment 3/Report/create.sh
@@ -0,0 +1,1 @@
+pandoc --template=default.latex  --listings -H listings-setup.tex -o Report.pdf Introduktion.md Teori.md Implementation.md Testing.md Reflektion.md Konklusion.md Sourcecode.md --number-sections --toc
